505b094cfa28e8a024724a286c24aff64255672a
Andres Aranda <andres.w.aranda@gmail.com>
2020-10-28 09:43:56 -0300

    Minipush

--- TPL06/UI.Desktop/Personas.Designer.cs
+++ TPL06/UI.Desktop/Personas.Designer.cs
@@ -29,27 +29,37 @@
         private void InitializeComponent()
         {
             this.dgvPersonas = new System.Windows.Forms.DataGridView();
+            this.Editar = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvPersonas)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvPersonas
             // 
             this.dgvPersonas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Editar});
             this.dgvPersonas.Location = new System.Drawing.Point(11, 11);
-            this.dgvPersonas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dgvPersonas.Margin = new System.Windows.Forms.Padding(2);
             this.dgvPersonas.Name = "dgvPersonas";
             this.dgvPersonas.RowHeadersWidth = 51;
             this.dgvPersonas.RowTemplate.Height = 24;
             this.dgvPersonas.Size = new System.Drawing.Size(578, 344);
             this.dgvPersonas.TabIndex = 0;
             // 
+            // Editar
+            // 
+            this.Editar.DataPropertyName = "Id";
+            this.Editar.HeaderText = "Editar";
+            this.Editar.Name = "Editar";
+            this.Editar.ReadOnly = true;
+            // 
             // Personas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(606, 402);
             this.Controls.Add(this.dgvPersonas);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Personas";
             this.Text = "Persona";
             this.Load += new System.EventHandler(this.Personas_Load);
@@ -61,5 +71,6 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dgvPersonas;
+        private System.Windows.Forms.DataGridViewButtonColumn Editar;
     }
 }
