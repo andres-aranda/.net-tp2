e30a7b1e47f19bcc7d884d5e3509306dca08276c
Braian Villasanti <villasantibraian@icloud.com>
2020-11-11 11:05:36 -0300

    elijo mas cosos

--- TPL06/UI.Web/Especialidades.aspx.cs
+++ TPL06/UI.Web/Especialidades.aspx.cs
@@ -7,8 +7,8 @@ using System.Web.UI.WebControls;
 using Business.Entities;
 using Business.Logic;
 
-// TODO: Estilo de tabla
-// TODO: Botones ABM
+// TODO: Estilo de tabla BRAIAN
+// TODO: Botones ABM BRAIAN
 
 namespace Academia.UI.Web
 {
